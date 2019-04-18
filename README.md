8111dda4af136ba1e26f5017064b8a7842cd8f3e
Ludovit Scholtz <ludovit.scholtz@wood.cz>
2019-04-18 11:39:14 +0200

    Add sample application

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # NumericUpDown Progress DataGridViewColumn
 This project enables to add numeric up down datagrid view column with progress bar
+
+![Sample application](https://raw.githubusercontent.com/scholtz/NumericUpDownProgressDataGridViewColumn/master/Img/Sample01.png)
---
+++ Sample/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public class SampleClass
+        {
+            public string Name { get; set; }
+            public decimal Value { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.Columns.Add(new DataGridViewRolloverCell())
+
+            sampleClassBindingSource.Add(new SampleClass() { Name = "First", Value = 15M });
+            sampleClassBindingSource.Add(new SampleClass() { Name = "Second", Value = 90M });
+            sampleClassBindingSource.Add(new SampleClass() { Name = "Third", Value = 30M });
+        }
+    }
+}
